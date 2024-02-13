4020ab15fc50300486f11d3cb1f2d09415fb9bb6
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-02-13 20:27:52 +0300

    Сделано 2 задание

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int result = 0;
+
+            foreach (int x in nums)
+            {
+                result += x;
+                Console.WriteLine(result);
+            }
+
+            List<int> list = new List<int>(nums);
+
+            List<int> list2 = new List<int>();
+
+            var input = from i in list
+                        where i %2 == 0
+                        select i;
+            
+            Console.WriteLine("LINQ MOMENT");
+            foreach (var item in input)
+            {
+                Console.WriteLine(item);
+            }   
+           
+            Console.ReadKey();
+        }
+    }
+}
